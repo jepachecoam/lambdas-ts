cc4a9252c2fa076b5b56086ce6b76a915df73d5b
jepachecoam <javierpacheco@bemaster.com>
2025-06-11 18:00:03 -0500

    feat: add log of event in Dto

--- src/lambdas/processAdditionalStepsInOrdersUpdate/dto/sharedDto.ts
+++ src/lambdas/processAdditionalStepsInOrdersUpdate/dto/sharedDto.ts
@@ -2,6 +2,7 @@ const extractParamsFromEvent = (event: any) => {
   const eventProcess = event["eventProcess"];
   const carrier = String(event["detail-type"]).toLocaleLowerCase();
   const detail = event["detail"];
+  console.log("event =>>>", JSON.stringify(event, null, 2));
   return {
     carrier,
     detail,
