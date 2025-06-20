9501a34b971bde0dc7676304b0a5e93cdbf23e70
jepachecoam <javierpacheco@bemaster.com>
2025-06-20 12:16:55 -0500

    drop unnecesary excel func

--- src/lambdas/example/index.ts
+++ src/lambdas/example/index.ts
@@ -1,4 +1,20 @@
-import { handler as excelReport } from "./excelReport/index";
-// import { handler as standardHandler } from "./standar";
+import httpResponse from "../../shared/responses/http";
+export const handler = async (
+  event: unknown,
+  _context: unknown
+): Promise<any> => {
+  try {
+    console.log("Event =>>>", event);
 
-export const handler = excelReport;
+    return httpResponse({
+      statusCode: 200,
+      body: "hello word from lambda"
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    return httpResponse({
+      statusCode: 500,
+      body: error
+    });
+  }
+};
