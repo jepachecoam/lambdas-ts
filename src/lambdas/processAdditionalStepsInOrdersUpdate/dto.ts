da0de3af3533afdb83dd1dab22eae9dddf8e55f2
jepachecoam <javierpacheco@bemaster.com>
2025-09-01 11:48:55 -0500

    add env validation

--- src/lambdas/processAdditionalStepsInOrdersUpdate/dto.ts
+++ src/lambdas/processAdditionalStepsInOrdersUpdate/dto.ts
@@ -3,7 +3,10 @@ const extractParamsFromEvent = (event: any) => {
   const eventProcess = event["eventProcess"];
   const carrier = String(event["detail-type"]).toLocaleLowerCase();
   const detail = event["detail"];
-  const environment = detail.environment ?? "prod";
+  let environment = detail.environment ?? "prod";
+  if (!["qa", "dev"].includes(environment)) {
+    environment = "prod";
+  }
   return {
     carrier,
     detail,
